d35c3dc04bb74c091c2d1e05b336eb79ed7dc758
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-01-17 14:23:48 +0900

    feat : 첫번째 커밋
    첫번째 커밋

---
+++ Demo1/Assets/NetworkManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public InputField NickNameInput;
+    public GameObject DisconnectPanel;
+    public GameObject ButtonManager;
+    
+    public GameObject ButtonCanvas;
+
+  
+    void Awake()
+    {
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 6 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        DisconnectPanel.SetActive(false);
+        ButtonManager.SetActive(true);
+        ButtonCanvas.SetActive(true);
+       
+        Spawn();
+    }
+
+
+    public void Spawn()
+    {
+        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+       
+    }
+
+    //게임에서 나가지기를 만들기
+    void Update() {
+         if (Input.GetKeyDown(KeyCode.Escape) && PhotonNetwork.IsConnected)
+         {
+            PhotonNetwork.Disconnect();
+         } 
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        //DisconnectPanel.SetActive(true);
+        ButtonManager.SetActive(false);
+        ButtonCanvas.SetActive(false);
+       
+    }
+}
+
+
+
+
